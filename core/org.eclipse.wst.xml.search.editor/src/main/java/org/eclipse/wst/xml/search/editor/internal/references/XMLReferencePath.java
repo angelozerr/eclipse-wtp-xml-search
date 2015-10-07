7a560f9aef50f8560d012e37e2de96018c89da1a
angelozerr <angelo.zerr@gmail.com>
2015-10-07 15:21:43 +0200

    Support for XPath /a[@name='xxx']

--- core/org.eclipse.wst.xml.search.editor/src/main/java/org/eclipse/wst/xml/search/editor/internal/references/XMLReferencePath.java
+++ core/org.eclipse.wst.xml.search.editor/src/main/java/org/eclipse/wst/xml/search/editor/internal/references/XMLReferencePath.java
@@ -72,6 +72,8 @@ public class XMLReferencePath extends AbstractXMLReferenceTo implements
 	}
 
 	private String computeXPathWithWildcard(String path) {
+		// replace simple quote with double quote to support from/to path like /a[@name='xxx']
+		path = path.replaceAll("[']", "\"");
 		if (nbWildCard == -1)
 			return path;
 		for (int i = 0; i < nbWildCard + 1; i++) {
